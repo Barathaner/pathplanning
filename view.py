b0d6519e92bba933763b14b292676ce2f97c8b31
Karl-Augustin Jahnel <freelance@karlaugustinjahnel.de>
2023-11-29 23:10:22 +0100

    5 zahlen

--- view.py
+++ view.py
@@ -1,6 +1,32 @@
-from PyQt5.QtWidgets import QMainWindow, QApplication, QGraphicsScene, QGraphicsView
+from PyQt5.QtWidgets import QDialog, QVBoxLayout, QLineEdit, QLabel, QPushButton, QApplication, QMainWindow, QGraphicsScene, QGraphicsView
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPolygonF, QPen, QColor
+
+class NumbersInputDialog(QDialog):
+    def __init__(self, parent=None):
+        super(NumbersInputDialog, self).__init__(parent)
+
+        self.setWindowTitle('Zahlen eingeben')
+
+        self.layout = QVBoxLayout(self)
+
+        # Labels und LineEdits für die Zahlen
+        self.lineEdits = []
+        for i in range(5):
+            self.layout.addWidget(QLabel(f'Zahl {i + 1}:'))
+            lineEdit = QLineEdit(self)
+            self.lineEdits.append(lineEdit)
+            self.layout.addWidget(lineEdit)
+
+        # OK und Cancel Buttons
+        self.buttons = QPushButton('OK', self)
+        self.buttons.clicked.connect(self.accept)
+        self.layout.addWidget(self.buttons)
+
+        self.setLayout(self.layout)
+
+    def getNumbers(self):
+        return [lineEdit.text() for lineEdit in self.lineEdits]
 
 class PolygonView(QMainWindow):
     def __init__(self, controller):
@@ -20,22 +46,28 @@ class PolygonView(QMainWindow):
         self.view.setMouseTracking(True)
         self.view.mousePressEvent = self.mousePressEvent
 
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_X:
+            self.openNumbersDialog()
+
+    def openNumbersDialog(self):
+        dialog = NumbersInputDialog(self)
+        if dialog.exec_():
+            numbers = dialog.getNumbers()
+            print(numbers)  # Hier können Sie etwas mit den Zahlen machen
+
     def mousePressEvent(self, event):
-        # Umwandlung der Mausposition in Szenen-Koordinaten
         scenePos = self.view.mapToScene(event.pos())
 
         x = scenePos.x()
         y = scenePos.y()
         self.controller.add_vertex(x, y)
 
-
     def draw_polygon(self, vertices):
-        self.scene.clear()  # Klären Sie die Szene, bevor Sie neu zeichnen
+        self.scene.clear()
 
         if vertices:
-            # Erstellen eines QPolygonF-Objekts mit den Vertex-Koordinaten
             polygon = QPolygonF(vertices)
 
             pen = QPen(QColor(Qt.red))
             self.scene.addPolygon(polygon, pen)
-
